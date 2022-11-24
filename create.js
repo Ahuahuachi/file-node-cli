6c789a9aebee72e74f67a1e858111aada56d7d39
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-11-23 19:58:42 -0600

    Fix data error

--- create.js
+++ create.js
@@ -1,7 +1,7 @@
 const fs = require("fs"); // CommonJS
 
 const path = process.argv[2];
-const content = process.argv[3];
+const content = process.argv[3] ? process.argv[3] : "";
 
 fs.writeFile(path, content, {}, (err) => {
   if (err) console.error("No se pudo escribir el archivo", err);

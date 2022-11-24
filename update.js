d385798f96fe41245e39f91fa5d29d2a298b049a
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-11-23 20:12:33 -0600

    Add update script

--- update.js
+++ update.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 
-fs.appendFile(
-  "firstNodeFile.txt",
-  "\nMás datos",
-  { encoding: "utf-8" },
-  (err) => {
-    if (err) console.error("No se pudo editar el archivo", err);
+const path = process.argv[2];
+const data = process.argv[3];
+
+if (data) {
+  fs.appendFile(path, `\n${data}`, { encoding: "utf-8" }, (err) => {
+    if (err) console.error(`No se pudo editar el archivo ${path}`, err);
     else console.log("Archivo modificado");
-  }
-);
+  });
+} else {
+  console.error("Los datos para modificar son obligatorios");
+}

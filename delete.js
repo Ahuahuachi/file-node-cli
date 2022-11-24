f74c60fe14e81db3b4d92ab4fa2c3d17a26a273a
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-11-23 20:15:31 -0600

    Add delete script

--- delete.js
+++ delete.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 
-fs.unlink("firstNodeFile.txt", (err) => {
-  if (err) console.error("No se pudo eliminar el archivo");
-  else console.log("Archivo eliminado exitosamente", err);
-});
+const path = process.argv[2];
+
+if (!path) {
+  console.error("La ruta del archivo a eliminar es obligatoria");
+} else {
+  fs.unlink(path, (err) => {
+    if (err) console.error("No se pudo eliminar el archivo", err);
+    else console.log("Archivo eliminado exitosamente");
+  });
+}

274b7b1d57a9a114a1a7945dcf1fd4dedf3ca4c7
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-11-23 20:04:05 -0600

    Add read script

--- read.js
+++ read.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
-fs.readFile("firstNodeFile.txt", { encoding: "utf-8" }, (err, data) => {
+const path = process.argv[2];
+
+fs.readFile(path, { encoding: "utf-8" }, (err, data) => {
   if (err) console.error("No se pudo leer el archivo", err);
   else console.log(data);
 });
